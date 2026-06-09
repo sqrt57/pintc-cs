3b26bef42023c8686b54915953ce25ec7953c3e6
Dmitry Grigoryev <1461123+sqrt57@users.noreply.github.com>
2026-06-09 10:29:26 +0200

    Merge Slice4PrecedenceTests into Slice4Tests

--- Pintc.E2eTests/Slice4Tests.cs
+++ Pintc.E2eTests/Slice4Tests.cs
@@ -23,4 +23,18 @@ public class Slice4Tests : IDisposable
         var run = CompilerRunner.Execute(exePath);
         run.ExitCode.ShouldBe(0);
     }
+
+    [Fact]
+    public void Precedence_compiles_and_exits_with_0()
+    {
+        var sourcePath = Path.Combine(_tempDir, "slice4-precedence.pnt");
+        var exePath    = Path.Combine(_tempDir, "slice4-precedence.exe");
+        File.WriteAllText(sourcePath, SliceFixtures.Slice4PrecedenceSource);
+
+        var compile = CompilerRunner.Compile(sourcePath, exePath);
+        compile.ExitCode.ShouldBe(0, compile.Stderr);
+
+        var run = CompilerRunner.Execute(exePath);
+        run.ExitCode.ShouldBe(0);
+    }
 }
