8a6bd29b0c7ef33ea3e961aa2c3eee282460fafc
Dmitry Grigoryev <1461123+sqrt57@users.noreply.github.com>
2026-06-09 10:54:47 +0200

    Add Slice 6 fixture and e2e test (failing)

---
+++ Pintc.E2eTests/Slice6Tests.cs
@@ -0,0 +1,26 @@
+using Pintc.E2eTests.Helpers;
+using Pintc.TestFixtures;
+
+namespace Pintc.E2eTests;
+
+public class Slice6Tests : IDisposable
+{
+    readonly string _tempDir = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+
+    public Slice6Tests() => Directory.CreateDirectory(_tempDir);
+    public void Dispose() => Directory.Delete(_tempDir, recursive: true);
+
+    [Fact]
+    public void Compiles_and_exits_with_0()
+    {
+        var sourcePath = Path.Combine(_tempDir, "slice6.pnt");
+        var exePath    = Path.Combine(_tempDir, "slice6.exe");
+        File.WriteAllText(sourcePath, SliceFixtures.Slice6Source);
+
+        var compile = CompilerRunner.Compile(sourcePath, exePath);
+        compile.ExitCode.ShouldBe(0, compile.Stderr);
+
+        var run = CompilerRunner.Execute(exePath);
+        run.ExitCode.ShouldBe(0);
+    }
+}
