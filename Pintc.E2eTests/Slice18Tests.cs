02ce7e5c87a61767e6b54eb6d203f122d442d69a
Dmitry Grigoryev <1461123+sqrt57@users.noreply.github.com>
2026-06-17 19:49:58 +0200

    Slice 18: e2e test fixtures and test class for named return values

---
+++ Pintc.E2eTests/Slice18Tests.cs
@@ -0,0 +1,54 @@
+using Pintc.E2eTests.Helpers;
+using Pintc.TestFixtures;
+
+namespace Pintc.E2eTests;
+
+public class Slice18Tests : IDisposable
+{
+    readonly string _tempDir = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+
+    public Slice18Tests() => Directory.CreateDirectory(_tempDir);
+    public void Dispose() => Directory.Delete(_tempDir, recursive: true);
+
+    [Fact]
+    public void Named_return_statement()
+    {
+        var sourcePath = Path.Combine(_tempDir, "slice18_named_return.pnt");
+        var exePath    = Path.Combine(_tempDir, "slice18_named_return.exe");
+        File.WriteAllText(sourcePath, SliceFixtures.Slice18NamedReturnSource);
+
+        var compile = CompilerRunner.Compile(sourcePath, exePath);
+        compile.ExitCode.ShouldBe(0, compile.Stderr);
+
+        var run = CompilerRunner.Execute(exePath);
+        run.ExitCode.ShouldBe(0);
+    }
+
+    [Fact]
+    public void Named_unpack()
+    {
+        var sourcePath = Path.Combine(_tempDir, "slice18_named_unpack.pnt");
+        var exePath    = Path.Combine(_tempDir, "slice18_named_unpack.exe");
+        File.WriteAllText(sourcePath, SliceFixtures.Slice18NamedUnpackSource);
+
+        var compile = CompilerRunner.Compile(sourcePath, exePath);
+        compile.ExitCode.ShouldBe(0, compile.Stderr);
+
+        var run = CompilerRunner.Execute(exePath);
+        run.ExitCode.ShouldBe(0);
+    }
+
+    [Fact]
+    public void Named_declaration_and_unpack()
+    {
+        var sourcePath = Path.Combine(_tempDir, "slice18_named_decl.pnt");
+        var exePath    = Path.Combine(_tempDir, "slice18_named_decl.exe");
+        File.WriteAllText(sourcePath, SliceFixtures.Slice18NamedDeclSource);
+
+        var compile = CompilerRunner.Compile(sourcePath, exePath);
+        compile.ExitCode.ShouldBe(0, compile.Stderr);
+
+        var run = CompilerRunner.Execute(exePath);
+        run.ExitCode.ShouldBe(0);
+    }
+}
